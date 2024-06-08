848a42e21ee41cef8e5e2e539be74b1d34f6aab3
AraMatevosyan <128417154+AraMatevosyan@users.noreply.github.com>
2024-06-08 15:03:05 +0400

    implement redux (#5)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
+import store from "./redux";
+import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
 if (container) {
   const root = ReactDOMClient.createRoot(container);
 
-  root.render(<App />);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
 }
